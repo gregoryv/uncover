9640182772f65950f5404e5b570ea9aef5633b70
Gregory Vincic <g@7de.se>
2020-01-04 22:16:05 +0100

    Remove unused code
    Started it to long ago, needs restart.

--- onchange.sh
+++ onchange.sh
@@ -9,11 +9,9 @@ nameonly="${filename%.*}"
 case $extension in
     go)
         goimports -w $path
-        gofmt -w $path
         ;;
 esac
 
-go install github.com/gregoryv/uncover/cmd/uncover
-go test -v -coverprofile /tmp/c.out -run=TestParseProfile2* .
-#go tool cover -o /tmp/coverage.html -html /tmp/c.out
-uncover /tmp/c.out 
+go install ./cmd/uncover
+go test -v -coverprofile /tmp/c.out .
+uncover /tmp/c.out
--- unit_test.go
+++ unit_test.go
@@ -1,7 +1,6 @@
 package uncover
 
 import (
-	"bytes"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -44,33 +43,5 @@ func TestParseProfiles(t *testing.T) {
 	exp := 2
 	if len(profiles) != exp { // files a.go and b.go
 		t.Errorf("Expected %v, got %v", exp, len(profiles))
-	}
-}
-
-func TestParseProfile2_badmodes(t *testing.T) {
-	cases := []string{"", "mode", "mode:", "mode: setj"}
-	for _, c := range cases {
-		r := bytes.NewBufferString(c)
-		err := ParseProfile2(r)
-		if err == nil {
-			t.Errorf("Expected to fail on %q with bad mode", c)
-		}
-	}
-}
-
-func TestParseProfile2_ok(t *testing.T) {
-	cases := []string{
-		"mode: set",
-		`mode: set
-github.com/gregoryv/uncover/test/a.go:3.16,5.2 1 0
-github.com/gregoryv/uncover/test/b.go:3.10,5.2 1 1
-`,
-	}
-	for _, c := range cases {
-		r := bytes.NewBufferString(c)
-		err := ParseProfile2(r)
-		if err != nil {
-			t.Errorf("Expected %q to be ok, got: %v", c, err)
-		}
 	}
 }
